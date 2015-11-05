d0fb4edcebbf92beb83a51388315ce4558496953
Jordan Matthiesen <jmatthiesen@gmail.com>
2015-11-04 17:13:39 -0800

    Removing a bogus folder.

--- src/VSTACOLive/live.asp.net/wwwroot/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../../gulpfile.js" />
-/// <reference path="../../live/js/site.js" />
-/// <reference path="../../live/lib/bootstrap/dist/js/bootstrap.js" />
-/// <reference path="../../live/lib/jquery/dist/jquery.js" />
-/// <reference path="../../live/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
